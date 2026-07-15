c4e67cda77f066b7c32072f369531e5f75eeb417
Ahmad Khan <ahmad@example.com>
2026-07-15 16:44:56 +0530

    Disable Tanstack React Query Devtools button

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -32,9 +32,7 @@ export default function QueryProvider({ children }: { children: React.ReactNode 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {process.env.NODE_ENV === "development" ? (
-        <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
-      ) : null}
+      {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" /> */}
     </QueryClientProvider>
   );
 }
